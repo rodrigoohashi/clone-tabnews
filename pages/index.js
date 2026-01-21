d749ab8fe70932efc52340f007a18ed76dcc8255
rodrigoohashi <rodrigoohashi@gmail.com>
2026-01-21 15:32:03 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Karina, eu amo você! Se você me ama, dê uma risadinha!!!</h1>
+  return <h1>Karina, eu amo você! Se você me ama, dê uma risadinha!!!</h1>;
 }
 
 export default Home;
+
+function teste2() {
+  console.log("indentação errada");
+}
